9270c44cd5b1e7ad7d22cae5c44ed9dc309ccf97
Aditya <aditya072@outlook.com>
2023-11-04 04:27:21 +0530

    first commit

---
+++ controllers/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+//get the date
+  const date = new Date();
+  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  console.log(date.getHours());
+  console.log(today.getHours());
